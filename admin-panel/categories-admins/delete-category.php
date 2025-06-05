a115ff5f58bccba6b07d03c0ce3b04fe33c22ebe
Long Pham <longpd1911@gmail.com>
2025-06-06 04:18:40 +0700

    admin categories

---
+++ admin-panel/categories-admins/delete-category.php
@@ -0,0 +1,20 @@
+<?php require "../layouts/header.php"; ?>  
+<?php require "../../config/config.php"; ?> 
+
+<?php
+    if(isset($_GET['id'])) {
+        $id = $_GET['id'];
+
+        $select = $conn->query("SELECT * FROM categories WHERE id = '$id'");
+        $select->execute();
+
+        $images = $select->fetch(PDO::FETCH_OBJ);
+        unlink("images/".$images->image."");
+
+        $delete = $conn->query("DELETE FROM categories WHERE id = '$id'");
+        $delete->execute();
+        header("Location: " . ADMINURL . "/categories-admins/show-categories.php"); 
+    } else {
+        header("Location: http://localhost/bookstore/404.php"); 
+    }
+?>
